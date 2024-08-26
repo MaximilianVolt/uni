1f3a4cd8d618aa96afcc9fb55b4bd2f28d70ae39
Maximilian Volt. <massimilianovolturno+git@gmail.com>
2024-08-26 10:31:52 +0200

    Made a library for it lmao

--- YEAR_01/C/_00_PRELIMINARY/ex03_aMacroProblem/main.c
+++ YEAR_01/C/_00_PRELIMINARY/ex03_aMacroProblem/main.c
@@ -8,29 +8,7 @@
 
 
 
-#define no_cap    1
-#define cap       0
-#define be        ==
-#define rn        ;
-#define chief     main
-#define no        !
-#define sussin    ?
-#define bussin    :
-#define yikes     break;
-#define deadass   void
-#define vibin     --
-#define ongod     ++
-#define fr        long
-#define bouta     &
-#define finna     =
-#define tho       {
-#define gyatt     }
-#define rizzler   int
-#define lowkey    return
-#define skibidi   -1.2f
-#define sigma     char
-#define ohio      *
-#define fanum     printf
+#include "./lib/library.h"
 
 
 
